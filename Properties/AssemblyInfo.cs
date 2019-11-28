597cb8b1308b105174cddce234b47c2702396695
OzNetMaster <ncolvin@nivloc.com>
2019-11-28 06:45:59 -0500

    Revert "Revert "Initial commit""
    This reverts commit 69d73392d08478161fcbd544725e7264e34d18e2.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle ("SSharpCrestronIOExtensionsLibrary")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SSharpCrestronIOExtensionsLibrary")]
+[assembly: AssemblyCopyright ("Copyright ©  2019")]
+[assembly: AssemblyVersion ("1.0.0.*")]
+
+[assembly: CLSCompliant(false)]
+
+
